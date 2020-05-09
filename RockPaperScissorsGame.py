a913964f634c74ee1c8e5bb98c25d6fef8103f58
PUNEET SINGHAL <puneet.singhal80@yahoo.com>
2020-05-09 11:35:58 +0530

    Rock Paper Scissors game update.

--- RockPaperScissorsGame.py
+++ RockPaperScissorsGame.py
@@ -1,33 +1,34 @@
 def main():
-    
-    Player_1 = input("Enter your name : ")
-    Player_2 = input("Enter your name : ")
+    player_1 = input("Enter your name : ")
+    player_2 = input("Enter your name : ")
 
-    P1 = input("Select one from Rock Paper Scissors : ")
-    P2 = input("Select one from Rock Paper Scissors : ")
+    p1 = input("Select one from Rock Paper Scissors : ")
+    p2 = input("Select one from Rock Paper Scissors : ")
 
-    P1.lower()
-    P2.lower()
-        
-    def winner(P1,P2):
-        if P1=="rock" and P2=="paper":
-            return("Player2 Wins")
-        elif P1== "rock" and P2== "scissors":
-            return("Player1 Wins")
-        elif P1== "paper" and P2== "rock":
-            return("Player1 wins")    
-        elif P1== "paper" and P2== "scissors":
-            return("player2 wins")
-        elif P1 == "scissors" and P2 == "rock":
-            return("Player2 Wins")
-        elif P1 == "scissors" and P2 == "paper":
-            return("Player1 wins")
+    p1.lower()
+    p2.lower()
 
-    print(winner(P1,P2))
+    def winner(input_1, input_2):
+        if input_1 == "rock" and input_2 == "paper":
+            return f"{player_1} Wins"
+        elif input_1 == "rock" and input_2 == "scissors":
+            return f"{player_1} Wins"
+        elif input_1 == "paper" and input_2 == "rock":
+            return f"{player_1} Wins"
+        elif input_1 == "paper" and input_2 == "scissors":
+            return f"{player_2} Wins"
+        elif input_1 == "scissors" and input_2 == "rock":
+            return f"{player_2} Wins"
+        elif input_1 == "scissors" and input_2 == "paper":
+            return f"{player_2} Wins"
+
+    print(winner(p1, p2))
     play_again = input("play again ? 1: Press 1 to Play again , 2: Press 2 to Exit")
     if play_again == "1":
         main()
     else:
         exit()
+
+
 if __name__ == "__main__":
     main()
